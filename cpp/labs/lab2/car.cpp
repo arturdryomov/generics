a74887d39ae2b860857ad145675ed449b4860617
Artur Dryomov <me@ming13.ru>
2011-06-04 16:44:05 +0300

    Added second lab of OOP

---
+++ cpp/labs/lab2/car.cpp
@@ -0,0 +1,117 @@
+#include "car.h"
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+void Cars::enterCarsInformation()
+{
+  cout << "Do you want enter information from file? (y/n): ";
+  char key;
+  cin >> key;
+  if (toupper(key) == 'Y') {
+    cout << "Enter input file name: ";
+    string filename;
+    cin >> filename;
+    enterFromFile(filename);
+  }
+  else {
+    enterFromStdio();
+  }
+}
+
+void Cars::enterFromFile(string filename)
+{
+  fstream inputFile(filename.c_str());
+
+  while (inputFile.good()) {
+    vector<string> inputParts;
+    inputParts.resize(5);
+
+    for (vector<string>::iterator it = inputParts.begin(); it != inputParts.end(); ++it) {
+      if (!inputFile.good()) {
+        return;
+      }
+      inputFile >> *it;
+    }
+
+    addCar(inputParts);
+  }
+
+  inputFile.close();
+}
+
+void Cars::enterFromStdio()
+{
+  cout << "Enter number of records: ";
+  int size;
+  cin >> size;
+  cout << "Now enter information in format:" << endl;
+  cout << " brand model [serial], year, color" << endl;
+
+  for (int i = 0; i < size; i++) {
+    vector<string> inputParts;
+    inputParts.resize(5);
+    for (vector<string>::iterator it = inputParts.begin(); it != inputParts.end(); ++it) {
+      cin >> *it;
+    }
+    addCar(inputParts);
+  }
+}
+
+void Cars::addCar(vector<string> inputParts)
+{
+  Car car;
+
+  // lexical_cast from Boost will be better
+  car.brand = inputParts[0];
+
+  car.model = inputParts[1];
+
+  inputParts[2].erase(inputParts[2].begin(), inputParts[2].begin() + 1);
+  inputParts[2].erase(inputParts[2].end() - 2, inputParts[2].end());
+  istringstream ( inputParts[2] ) >> car.serial;
+
+  inputParts[3].erase(inputParts[3].end() - 1, inputParts[3].end());
+  istringstream ( inputParts[3] ) >> car.year;
+
+  istringstream ( inputParts[4] ) >> car.color;
+
+  m_carsList.push_back(car);
+}
+
+void Cars::sortCarsList()
+{
+  sort(m_carsList.begin(), m_carsList.end(), Cars::compareYear);
+  sort(m_carsList.begin(), m_carsList.end(), Cars::compareBrand);
+}
+
+bool Cars::compareYear(Car first, Car second)
+{
+  return (first.year < second.year);
+}
+
+bool Cars::compareBrand(Car first, Car second)
+{
+  if (second.brand.compare(first.brand) < 0) {
+    return 0;
+  }
+  return (second.brand.compare(first.brand));
+}
+
+void Cars::printCarsInformation()
+{
+  for (vector<Car>::iterator it = m_carsList.begin(); it != m_carsList.end(); ++it) {
+    cout << setw(16) << (*it).brand 
+         << setw(9) << (*it).model 
+         << setw(8) << (*it).serial 
+         << setw(5) << (*it).year
+         << setw(4) << (*it).color << endl;
+  }
+}
---
+++ cpp/labs/lab2/car.h
@@ -0,0 +1,32 @@
+#ifndef CAR_H
+#define CAR_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Car {
+  string brand;
+  string model;
+  int serial;
+  int year;
+  int color;
+};
+
+class Cars {
+public:
+  void enterCarsInformation();
+  void sortCarsList();
+  void printCarsInformation();
+private:
+  vector<Car> m_carsList;
+
+  void enterFromFile(string filename);
+  void enterFromStdio();
+  void addCar(vector<string> inputParts);
+  static bool compareYear(const Car first, const Car second);
+  static bool compareBrand(const Car first, const Car second);
+};
+
+#endif // CAR_H
