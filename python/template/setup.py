648c47d209c202f70914e23a56409e44b220c2dc
Artur Dryomov <me@ming13.ru>
2011-12-13 19:33:44 +0300

    Added template of Python package uses setuptools.

---
+++ python/template/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "template",
+    version = "0.0.1",
+    author = "Artur Dryomov",
+    author_email = "me@ming13.ru",
+    description = "Template project",
+    license = "BSD",
+    url = "https://github.com/ming13/playground/python/template",
+    packages = find_packages(),
+    classifiers = [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Utilities",
+    ],
+)
